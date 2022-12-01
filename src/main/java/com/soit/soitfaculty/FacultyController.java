9a645044d720dd8ea7ce4555228fd9a20e3ed5b1
Pranav Mahajan <pranavm7@outlook.com>
2022-12-01 13:41:41 -0500

    Add button configured.

--- src/main/java/com/soit/soitfaculty/FacultyController.java
+++ src/main/java/com/soit/soitfaculty/FacultyController.java
@@ -8,6 +8,8 @@ import javax.annotation.PostConstruct;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.soit.soitfaculty.entity.Faculty;
@@ -38,6 +40,36 @@ public class FacultyController {
 	public String listFaculties(Model theModel) {
 		// Add faculties to Spring Model
 		theModel.addAttribute("faculties", theFaculties);
-		return "list-faculties";
+		return "faculties/list-faculties";
+	}
+
+	@GetMapping("/viewAddForm")
+	public String viewAddForm(Model theModel) {
+		
+		
+		
+		//Model attribute for data binding
+		Faculty theFaculty = new Faculty();
+		
+		theModel.addAttribute("faculty", theFaculty);
+		
+		
+		return "faculties/faculty-form";
+		
+	}
+	
+	
+	@PostMapping("/save")
+	public String saveFaculty(@ModelAttribute("faculty") Faculty theFaculty) {
+		
+		
+		
+		//Register the Faculty
+		facultyService.save(theFaculty);
+		
+		
+		//Block duplicates submission for accidental refresh
+		return "redirect:/Faculties/list";
+		
 	}
 }
