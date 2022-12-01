cfe94997b6c968e173271b134f7a373c7de4a718
Pranav Mahajan <pranavm7@outlook.com>
2022-12-01 13:45:07 -0500

    Edit + Delete buttons done. App complete!

--- src/main/java/com/soit/soitfaculty/FacultyController.java
+++ src/main/java/com/soit/soitfaculty/FacultyController.java
@@ -1,9 +1,6 @@
 package com.soit.soitfaculty;
 
-import java.util.ArrayList;
 import java.util.List;
-
-import javax.annotation.PostConstruct;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,36 +8,37 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.soit.soitfaculty.entity.Faculty;
+import com.soit.soitfaculty.service.FacultyService;
 
 @Controller
 @RequestMapping("/Faculties")
 public class FacultyController {
-	// Upload Faculty info
-	private List<Faculty> theFaculties;
 	
-	@PostConstruct
-	private void loadData() {
-		
-		// Create Faculties
-		Faculty fac1 = new Faculty(1, "Kelly", "Miller", "Kelly@uc.edu");
-		Faculty fac2 = new Faculty(2, "Robert", "Lee", "Robert@uc.edu");
-		Faculty fac3 = new Faculty(3, "Laura", "West", "Laura@uc.edu");
-		// Create our List
-		theFaculties = new ArrayList<>();
-		
-		// Add to our List
-		theFaculties.add(fac1);
-		theFaculties.add(fac2);
-		theFaculties.add(fac3);
+	
+	public FacultyService facultyService;
+	
+	public FacultyController (FacultyService theFacultyService){
+		facultyService = theFacultyService;
 	}
+	
 	//Mapping for "/list"
 	@GetMapping("/list")
 	public String listFaculties(Model theModel) {
-		// Add faculties to Spring Model
+		
+		
+		//Retrieve faculties from the Database
+		List<Faculty> theFaculties = facultyService.findAll();
+		
+		
+		//Add Faculties to the Spring Model
 		theModel.addAttribute("faculties", theFaculties);
+		
 		return "faculties/list-faculties";
+		
+		
 	}
 
 	@GetMapping("/viewAddForm")
@@ -59,6 +57,32 @@ public class FacultyController {
 	}
 	
 	
+	@GetMapping("/viewUpdateForm")
+	public String viewUpdateForm (@RequestParam("facultyId") int theId, Model theModel) {
+		
+		
+		
+		//Retrieve the faculty info from the service layer
+		Faculty theFaculty = facultyService.findById(theId);
+		
+		
+		
+		
+		//Pre-populate the form by setting the faculty as a model attribute
+		theModel.addAttribute("faculty", theFaculty);
+		
+		
+		
+		//Redirect us to the faculty form
+		return "faculties/faculty-form";
+		
+		
+		
+		
+	}
+	
+	
+	
 	@PostMapping("/save")
 	public String saveFaculty(@ModelAttribute("faculty") Faculty theFaculty) {
 		
@@ -72,4 +96,21 @@ public class FacultyController {
 		return "redirect:/Faculties/list";
 		
 	}
+	
+	@GetMapping("/delete")
+	public String delete(@RequestParam("facultyId") int theId) {
+		
+		
+		//Remove Faculty
+		facultyService.deleteById(theId);
+		
+		
+		//Return to the Faculties' directory
+		return "redirect:/Faculties/list";
+		
+		
+		
+		
+	}
+	
 }
